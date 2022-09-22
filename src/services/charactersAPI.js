78dfb88f5075be0a2a5675286d8cd96ef07d83a1
RamonaS2 <bandeiraramona2@gmail.com>
2022-09-22 10:52:39 -0300

    removendo app-react-url

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 require('dotenv/config');
 
-const { REACT_APP_URL } = process.env;
+// const { REACT_APP_URL } = process.env;
 
 const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = REACT_APP_URL, timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
